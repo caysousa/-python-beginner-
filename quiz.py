171952a19290ad1059a1badf6c61d6abebd37f26
Cayala Sousa <cayjng@gmail.com>
2025-01-22 10:17:46 -0300

    quiz

---
+++ quiz.py
@@ -0,0 +1,55 @@
+print ('Seja bem vindo ao quiz!')
+answer_user = input ('Gostaria de começar? (S/N) ')
+
+if answer_user != 'S':
+    quit ()
+
+print ('LETS START!')
+print ('Qual filme com "Miss" em seu nome tem como atriz Sandra Bullock? \n (a) Miss Hollywood \n (b) Miss Simpatia \n (c) Pequena Miss Sunshine')
+answer1 = input ('Resposta: ')
+
+if answer1 == 'b':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
+print ('Quem interpreta Hermione Granger na Saga Harry Potter? \n (a) Rachel Weisz \n (b) Naomi Watts \n (c) Emma Watson')
+answer2 = input ('Resposta: ')
+
+if answer2 == 'c':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
+print ('Qual dos personagens abaixo não usa máscara? \n (a) Leatherface \n (b) Samara \n (c) Ghostface')
+answer3 = input ('Resposta: ')
+
+if answer3 == 'b':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
+print ('Qual o nome do dragão do Soluço no filme "Como treinar seu Dragão"? \n (a) Banguela \n (b) Astrid \n (c) Melequento')
+answer4 = input ('Resposta: ')
+
+if answer4 == 'a':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
+print ('No filme "Invocação do Mal: 2" qual é o nome da Freira? \n (a) Lavak \n (b) Kaval \n (c) Valak')
+answer5 = input ('Resposta: ')
+
+if answer5 == 'c':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
+print ('Qual personagem mata as pessoas em seus sonhos? \n (a) Michael Myers \n (b) Jason Voorhees \n (c) Freddy Krueger')
+answer6 = input ('Resposta: ')
+
+if answer6 == 'c':
+    print ('CORRETO!')
+else:
+    print ('INCORRETO!')
+
